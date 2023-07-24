9b591f87379a42fc3420a6cfe1db1cadef4620ea
elviocepeda <cepedaelvio@gmail.com>
2023-07-24 12:34:12 -0300

    library/tracker-component-refactoring
    Created constant file to save mock data, interfaces, etc

---
+++ src/components/tracker/constants.ts
@@ -0,0 +1,38 @@
+import { Color } from "@tremor/react";
+
+export interface DataProps {
+  color: Color;
+  tooltip: string;
+}
+
+export interface TrackerProps {
+  data: DataProps[];
+}
+
+export const data: DataProps[] = [
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "rose", tooltip: "Downtime" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "gray", tooltip: "Maintenance" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "yellow", tooltip: "Degraded" },
+  { color: "emerald", tooltip: "Operational" },
+];
