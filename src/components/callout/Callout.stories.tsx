5631f6afc78a62eb0f13b3baa93240a41995667a
MatiasColatto <mimaco1234@gmail.com>
2023-07-25 11:58:24 -0300

    Adding some new variants

--- src/components/callout/Callout.stories.tsx
+++ src/components/callout/Callout.stories.tsx
@@ -1,7 +1,11 @@
-
 import type { Meta, StoryObj } from "@storybook/react";
 import {DefaultCallout  as Callout } from "./Callout";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleCheck } from '@fortawesome/pro-solid-svg-icons'
 
+const iconValue: React.FC = () => {
+    return <FontAwesomeIcon icon={faCircleCheck} size="xl" pull="left" ></FontAwesomeIcon>;
+  };
 
 const meta = {
  title: "Taggify/Callout/Callout",
@@ -19,6 +23,24 @@ type Story = StoryObj<typeof meta>;
 
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const DefaulDefaultCalloutBadge: Story = {
- args: { title: "Warning" , children: " :) "},
+export const DefaultCalloutS: Story = {
+ args: { title: "-" , children: " - "},
+};
+
+export const WarningCallout: Story = {
+    args: { 
+        title: "Oops! Something went wrong." , 
+        children: "We encountered an unexpected error while processing your request. Our team has been notified of the issue and is working diligently to fix it. Please try again later." , 
+        color: "red", 
+        icon: iconValue
+    },
+};
+
+   export const OKCallout: Story = {
+    args: { 
+        title: "No critical system data." , 
+        children: "All systems are currently within their default operating ranges." , 
+        color: "green", 
+        icon: iconValue
+    },
 };
